869e5a52f474b9663d76c2655d797fdd8a5dc64e
Andrew <aniemann1@gmail.com>
2021-06-03 19:29:46 -0400

    start getting a node backend in

---
+++ api/discord_auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const redirect = encodeURIComponent('http://localhost:30001/api/discord/callback');
+
+router.get('/login', (req, res) => {
+  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
+});
+
+module.exports = router;
